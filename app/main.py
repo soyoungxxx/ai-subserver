7a40308609df0eec3acccd73a35bca6c0ab88c32
soyoungxxx <otherplet@gmail.com>
2025-05-12 14:14:04 +0900

    feat: add logic to save uploaded image to local storage

--- app/main.py
+++ app/main.py
@@ -1,15 +1,25 @@
 from fastapi import FastAPI, UploadFile, File
 from models.yolo.yolo_model import YOLOModel
+import shutil
+import os
 
 app = FastAPI()
-model = YOLOModel("yolov8n.pt")
+# model = YOLOModel("yolov8n.pt")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+# yolo 추론 받는 코드
 @app.post("/predict/yolo")
 async def predict_yolo(file: UploadFile = File(...)):
-    contents = await file.read()
-    result_json = model.predict(contents)
-    return {"result": result_json}
+    # input / output 경로 지정
+    input_path = f"temp/input_{file.filename}";
+    output_path = f"temp/output_{file.filename}";
+    
+    # 디렉토리 없으면 생성
+    os.makedirs("temp", exist_ok=True)
+
+    # 파일 저장
+    with open(input_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer);
