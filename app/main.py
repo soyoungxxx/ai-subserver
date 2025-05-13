2adff86a036de77f8b12a1741f5a0b596077f55c
soyoungxxx <otherplet@gmail.com>
2025-05-13 10:57:58 +0900

    feat: add ROI region input handling from user

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Form
 from models.yolo.yolo_model import YOLOModel
+from schemas.yolo_schemas import Polygon
 import shutil
 import os
 
@@ -9,20 +10,29 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-# yolo 추론 받는 코드
+# yolo 일반 추론
 @app.post("/predict/yolo")
-async def predict_yolo(file: UploadFile = File(...)):
+async def predict_yolo(
+    file: UploadFile = File(...),
+    polygon_json: str = Form(...),
+):
     # input / output 경로 지정
     input_path = f"temp/input_{file.filename}";
     output_path = f"temp/output_{file.filename}";
-    
+
     # 디렉토리 없으면 생성
     os.makedirs("temp", exist_ok=True)
 
     # 파일 저장
     with open(input_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer);
+    
+    # json 파싱
+    polygon_data = Polygon.parse_raw(polygon_json);
 
     # 모델 호출
     model = YOLOModel();
     model.predict(input_path, output_path);
+
+    # output 디렉토리 내 결과 WAS로 전송
+    return {"output": output_path, "data": polygon_data};
